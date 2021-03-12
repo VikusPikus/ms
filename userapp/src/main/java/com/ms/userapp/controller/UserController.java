5c559dccfb952fe75b62d63e128e8145bd53932e
Viktoriia Moiseienko <Viktoriia_Moiseienko1@epam.com>
2021-03-12 13:02:16 +0200

    User - first commit

---
+++ userapp/src/main/java/com/ms/userapp/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.ms.userapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userservice")
+public class UserController {
+	
+	@GetMapping("/status")
+	public String status() {
+		return "userservice is up";
+	}
+
+}
